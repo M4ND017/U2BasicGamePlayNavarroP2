7c627dd6d00c1b64e3175e717ad1e728797d09c9
M4ND017 <732296@student.dusd.net>
2025-03-24 10:21:35 -0700

    2.0
    i almost finished the second lesson. i need two more videos.

--- BasicGamePlayU2NavarroP2/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayU2NavarroP2/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
 
     public float horizontalInput;
-    public float speed = 10.0f
+    public float speed = 10.0f;
+    public float xRange = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from a player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+            horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
     }
 }
